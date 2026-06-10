ad1b7e797682a2a369c9c3b8eee7dbbec2d11095
sferehi18 <sferehi18@gmail.com>
2026-06-10 15:38:52 +0200

    Ajuste para render

--- santiago-backend/main.py
+++ santiago-backend/main.py
@@ -26,7 +26,8 @@ import os
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 llm = ChatGroq(
     model_name="llama-3.1-8b-instant",
-    temperature=0.2
+    temperature=0.2,
+    api_key=GROQ_API_KEY
 )
 loader = TextLoader("contexto.txt", encoding="utf-8")
 documents = loader.load()
